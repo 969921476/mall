3b31a866e5acf7723a2be9d29e08a74898575d84
chenjunnan <m18460365733_1@163.com>
2021-01-12 18:07:50 +0800

    添加第三方应用模块

---
+++ mall-product/src/test/java/com/mall/product/controller/FileController.java
@@ -0,0 +1,14 @@
+package com.mall.product.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: ChenJunNan
+ * @Description:
+ * @Date: create in 2021/1/12 17:08
+ */
+@RestController
+@RequestMapping
+public class FileController {
+}
